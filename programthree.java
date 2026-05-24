46a5d4293b4d7b863191684dc171461b4c503593
Anup Yadav <anupyadav7409@gmail.com>
2026-05-24 17:34:00 +0530

    first commit

---
+++ programthree.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class programthree {
+
+    static int factorial(int n) {
+        int fact = 1;
+
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+        return fact;
+
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number = ");
+        int n = sc.nextInt();
+
+        if (n < 0) {
+            System.out.println("User entered negative number");
+        } else {
+            int result = factorial(n);
+
+            System.out.println("The factorial of the given number is = " + result);
+        }
+
+    }
+}
